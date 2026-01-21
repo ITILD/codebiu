3247afc7fd2077b9e1c6a0e183c388ce8df70ec1
lpp <geolifestudy@gmail.com>
2026-01-21 15:56:23 +0800

    feat(docker): 添加docker-compose相关脚本和配置文件
    添加restart.sh, clean.sh, start.sh, stop.sh脚本用于管理docker容器
    更新docker-compose.yml配置，添加nginx, postgis, redis等服务
    修改Dockerfile配置和构建脚本
    更新README.md中的构建和运行命令

--- code/backend/server_py/tools/docker_dev/build.sh
+++ code/backend/server_py/tools/docker_dev/build.sh
@@ -1,8 +1,8 @@
 
-PROJECT_NAME=base_server
+PROJECT_NAME=server_py
 PROJECT_VERSION_OLD=0.0.0
 PROJECT_VERSION=0.0.1
-# 先停止容器(如果正在运行) docker stop knowledge_1.0.1
+# 先停止容器(如果正在运行) docker stop server_py_1.0.1
 docker stop ${PROJECT_NAME}_${PROJECT_VERSION_OLD}
 
 # 删除旧容器
